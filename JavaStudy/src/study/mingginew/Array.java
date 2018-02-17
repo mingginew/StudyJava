d5eb77164b3af12e94ce0bde0c9f3a23b2046e35
Sherlock E. Gift <32980429+ClassOrder@users.noreply.github.com>
2018-02-17 11:54:37 +0900

    Create Array

---
+++ JavaStudy/src/study/mingginew/Array.java
@@ -0,0 +1,37 @@
+package study.mingginew;
+
+public class Array {
+	
+	public static void main(String[] args) {
+		
+		int []number = new int[5];
+		
+		number[0] = 1;
+		number[1] = 2;
+		number[2] = 3;
+		number[3] = 4;
+		number[4] = 5;
+		
+		
+		String []alphabet = new String[5];
+		
+		alphabet[0] = "A";
+		alphabet[1] = "B";
+		alphabet[2] = "C";
+		alphabet[3] = "D";
+		alphabet[4] = "F";
+		
+		int numbering[] = new int[5];
+		
+		
+		int j = 1;
+		for(int i = 0; i < numbering.length; i++) {
+			
+		}
+		
+		for(int i = 0; i < numbering.length; i++) {
+			
+		}
+	}
+
+}
